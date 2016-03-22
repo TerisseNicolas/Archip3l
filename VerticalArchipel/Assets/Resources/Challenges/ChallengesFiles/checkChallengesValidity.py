3146cd289be65cb3fa2ce2fca539a49cc89a0a31
Baptiste Amato <baptiste.amato@gmail.com>
2016-03-22 12:34:09 +0100

    started challenges files revision

--- VerticalArchipel/Assets/Resources/Challenges/ChallengesFiles/checkChallengesValidity.py
+++ VerticalArchipel/Assets/Resources/Challenges/ChallengesFiles/checkChallengesValidity.py
@@ -1,18 +1,33 @@
 buildingsList = ['GoldMine', 'StoneMine', 'OilPlant', 'Sawmill', 'Factory', 'WindTurbine', 'Farm', 'Harbor', 'PowerPlant', 'Lab', 'Airport', 'Hotel', 'School','Church', 'Cinema', 'AmusementPark']
 challengeTypesList = ['QCM', 'VraiFaux']
+
+#errors detection
 print('\n')
 for challengeType in challengeTypesList:
 	for building in buildingsList:
 		fileName = challengeType + "_" + building + ".txt"
 		challengeFile = open(challengeType + "/" + fileName, "r")
-		if (fileName == "VraiFaux_StoneMine.txt"):
-			for line in challengeFile.readlines():
-				splitted = line.split(';')
-				length = len(splitted)
-				if (challengeType == "QCM" and length != 1 and length != 6):
-					print("semicolon error in " + fileName)
-				if (challengeType == "VraiFaux" and length != 1 and length != 5):
-					print("semicolon error in " + fileName)
-				if ('\"' in line or '«' in line or'‘' in line):
-					print("quotation mark error in " + fileName)
+		for line in challengeFile.readlines():
+			splitted = line.split(';')
+			length = len(splitted)
+			if (challengeType == "QCM" and length != 1 and length != 6):
+				print("semicolon error in " + fileName)
+			if (challengeType == "VraiFaux" and length != 1 and length != 5):
+				print("semicolon error in " + fileName)
+			if ('\"' in line or '«' in line or'‘' in line):
+				print("quotation mark error in " + fileName)
+			if ('_' in line):
+				print("underscore present in " + fileName)
+		challengeFile.close
+print('\n')
+
+#number of lines printing
+
+print('\n')
+for challengeType in challengeTypesList:
+	for building in buildingsList:
+		fileName = challengeType + "_" + building + ".txt"
+		challengeFile = open(challengeType + "/" + fileName, "r")
+		print(challengeType + "/" + fileName + ": " + str(len(challengeFile.readlines())) + " questions")		
+		challengeFile.close
 print('\n')
