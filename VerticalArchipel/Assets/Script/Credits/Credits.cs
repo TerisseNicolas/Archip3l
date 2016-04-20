13973874c189515c0235058326c4bd6eef478f56
Terisse Nicolas <nicolas.terisse@gmail.com>
2016-04-21 00:08:22 +0200

    Music for credits..

---
+++ VerticalArchipel/Assets/Script/Credits/Credits.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        SoundPlayer.Instance.playBriefingLetterSound();
+	}
+}
