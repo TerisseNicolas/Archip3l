0b640bac46968f7d84bea7f2094551e291f3977a
nte <nicolas.terisse@gmail.com>
2017-04-16 19:55:04 +0200

    Xml reader

--- VerticalArchipel/Assets/Script/Game/ConstantsLoader.cs
+++ VerticalArchipel/Assets/Script/Game/ConstantsLoader.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System;
 using System.IO;
+using System.Xml.Serialization;
 
 public class ConstantsLoader : MonoBehaviour {
 
     public static Dictionary<TypeConstant, string> constantValues = new Dictionary<TypeConstant, string>();
-    private static string path = "data.txt";
+    private static string path = "parametres.xml";
 
 	void Awake()
     {
@@ -28,28 +29,24 @@ public class ConstantsLoader : MonoBehaviour {
 
     public static bool loadData()
     {
-        //Debug.Log("load");
-        string line = "   ";
-        Dictionary<TypeConstant, string> constantValuesTemp = new Dictionary<TypeConstant, string>();
-
-        if (File.Exists(ConstantsLoader.path))
+        String p = ConstantsLoader.path;
+        if (File.Exists(p))
         {
-            StreamReader file = new StreamReader(path);
-            while (((line = file.ReadLine()) != null)  && (line.Length > 2))
-            {
-                try
-                {
-                    constantValuesTemp.Add((TypeConstant)Enum.Parse(typeof(TypeConstant), line.Split('=')[0]), line.Split('=')[1]);
-                }
-                catch (ArgumentException)
-                {
-                    Debug.Log("An element with this key already exists.");
-                    return false;
-                }
+            ArchipelConstants constants = null;
+            XmlSerializer serializer = new XmlSerializer(typeof(ArchipelConstants));
+            StreamReader reader = new StreamReader(p);
+            constants = (ArchipelConstants)serializer.Deserialize(reader);
+            reader.Close();
 
-            }
-            file.Close();
-            ConstantsLoader.constantValues = constantValuesTemp;
+            ConstantsLoader.constantValues.Add(TypeConstant.networkListeningPort, constants.networkListeningPort.value);
+            ConstantsLoader.constantValues.Add(TypeConstant.networkSendingPort, constants.networkSendingPort.value);
+            ConstantsLoader.constantValues.Add(TypeConstant.networkServerIP, constants.networkServerIP.value);
+            ConstantsLoader.constantValues.Add(TypeConstant.fileScores, constants.fileScores.value);
+            ConstantsLoader.constantValues.Add(TypeConstant.pirateBoatsIncreaseRate, constants.pirateBoatsIncreaseTime.value);
+            ConstantsLoader.constantValues.Add(TypeConstant.pirateBoatsStart, constants.pirateBoatsStart.value);
+            ConstantsLoader.constantValues.Add(TypeConstant.timerChallenge, constants.timerChallenge.value);
+            ConstantsLoader.constantValues.Add(TypeConstant.timerDisturbance, constants.timerDisturbance.value);
+            ConstantsLoader.constantValues.Add(TypeConstant.timerGame, constants.timerGame.value);
             return true;
         }
         else
@@ -70,5 +67,44 @@ public class ConstantsLoader : MonoBehaviour {
             Debug.Log("Dictionary key not found.");
             return string.Empty;
         }
+    }
+
+    [Serializable()]
+    public class ArchipelConstantValue
+    {
+        [System.Xml.Serialization.XmlElement("Value")]
+        public string value { get; set; }
+    }
+
+    [Serializable()]
+    [System.Xml.Serialization.XmlRoot("Archipel")]
+    public class ArchipelConstants
+    {
+        [System.Xml.Serialization.XmlElement("networkListeningPort")]
+        public ArchipelConstantValue networkListeningPort { get; set; }
+
+        [System.Xml.Serialization.XmlElement("networkSendingPort")]
+        public ArchipelConstantValue networkSendingPort { get; set; }
+
+        [System.Xml.Serialization.XmlElement("networkServerIP")]
+        public ArchipelConstantValue networkServerIP { get; set; }
+
+        [System.Xml.Serialization.XmlElement("pirateBoatsStart")]
+        public ArchipelConstantValue pirateBoatsStart { get; set; }
+
+        [System.Xml.Serialization.XmlElement("pirateBoatsIncreaseTime")]
+        public ArchipelConstantValue pirateBoatsIncreaseTime { get; set; }
+
+        [System.Xml.Serialization.XmlElement("timerGame")]
+        public ArchipelConstantValue timerGame { get; set; }
+
+        [System.Xml.Serialization.XmlElement("timerChallenge")]
+        public ArchipelConstantValue timerChallenge { get; set; }
+
+        [System.Xml.Serialization.XmlElement("timerDisturbance")]
+        public ArchipelConstantValue timerDisturbance { get; set; }
+
+        [System.Xml.Serialization.XmlElement("fileScores")]
+        public ArchipelConstantValue fileScores { get; set; }
     }
 }
